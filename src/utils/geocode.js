c97852499d2fb0466a91375662c00c549a43c7fc
smartino555 <smartino555@gmail.com>
2024-08-27 14:34:30 -0400

    fixed another bug

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,19 +1,20 @@
 const request = require('postman-request');
 
 const geocode = (address, callback) => {
-    const url = 'https://api.positionstack.com/v1/forward?access_key=ea2121ec16ec64c3347e7ce112d910ec&query=' + encodeURIComponent(address);
+    const url = 'https://api.positionstack.com/v1/forward?access_key=ea2121ec16ec64c3347e7ce112d910ec&query=' + encodeURIComponent(address) + '&limit=1';
 
     request({ url: url, json: true }, (error, response) => {
         if (error) {
             callback('Unable to connect to location services', undefined);
-        } else if (response.body.features.length === 0) {
+        } else if (response.body.data.length === 0) {
             callback('Unable to find location. Try another search', undefined);
         } else {
-            const { properties } = response.body.data[0];
+            const { body } = response;
+            console.log(body.data[0]);
             callback(undefined, {
-                location: properties.label,
-                latitude: properties.latitude,
-                longitude: properties.longitude
+                location: body.data[0].label,
+                latitude: body.data[0].latitude,
+                longitude: body.data[0].longitude
             });
 
         }
